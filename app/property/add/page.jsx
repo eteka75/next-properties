c5ddd431ba3ea9dda495f708afe1e1f7c2101809
ETEKA Wilfried <etekawilfried@gmail.com>
2024-05-04 12:58:41 +0200

    Save: Home & Show properties

---
+++ app/property/add/page.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddProperty = () => {
+  return (
+    <div className="p-4 my-8">
+      <h1>AddProperty</h1>
+    </div>
+  );
+};
+
+export default AddProperty;
